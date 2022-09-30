38a0b178fce3c23dca03e2dee8b8405371ffbe36
chamokBarmon <chamokbarmon123@gmail.com>
2022-09-30 19:20:43 +0600

    all

--- src/componeds/Question/Question.js
+++ src/componeds/Question/Question.js
@@ -4,7 +4,15 @@ import './Question.css';
 const Question = () => {
     return (
         <div className='qus'>
-           
+            <p>1. How Does react work ?</p><br />
+            <p>Answer :ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
+             <br /><br />
+             <p> 2. props vs state </p>
+             <br />
+             <p>ans :Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components</p>
+             <br /><br />
+             <p> 3. useEffect uses react ?</p>
+             <p>ans :useEffect after render: We know that, the useEffect() is used for causing side effects in functional components and it is also capable for handling componentDidMount(), componentDidUpdate() and componentWillUnmount() life-cycle methods of class based components into functional component</p>
 
         </div>
     );
