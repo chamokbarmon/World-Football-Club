00e234d9d8db1b28fb1074ca3378a5efbcaf6d3a
chamokBarmon <chamokbarmon123@gmail.com>
2022-09-30 19:17:48 +0600

    eight

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Teams from './componeds/Teams/Teams';
 function App() {
   return (
     <div className="App">
-        <h1>World Football Club</h1>
-        <Teams></Teams>
-        <Question></Question>
+        
     </div>
   );
 }
